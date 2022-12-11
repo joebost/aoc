166711886035bfd8945fef196ba127ade42926a2
Joe Bost <joebost090@gmail.com>
2022-12-10 23:47:40 -0800

    day5

--- src/05.py
+++ src/05.py
@@ -1,5 +1,6 @@
 from utils.api import get_input
 import string
+import copy
 
 input_str = get_input(5)
 
@@ -24,9 +25,36 @@ n = {}
 for i in range(len(lines[-1])):
   c = lines[-1][i]
   if c != " ":
-    n[c] = m[i]
+    n[int(c)] = m[i][::-1]
+
+n2 = copy.deepcopy(n)
     
+for move in moves.split("\n"):
+  words = move.split(" ")
+  amount, fromm, to = int(words[1]), int(words[3]), int(words[5])
 
-print(n)
-#todo
+  for i in range(amount):
+    n[to].append(n[fromm].pop())
 
+for num, stack in n.items():
+  print(stack[-1], end="")
+
+print()
+
+n = n2
+
+# p2
+for move in moves.split("\n"):
+  words = move.split(" ")
+  amount, fromm, to = int(words[1]), int(words[3]), int(words[5])
+
+  to_move = []
+  for i in range(amount):
+    to_move.insert(0, n[fromm].pop())
+  
+  n[to] += to_move
+
+for num, stack in n.items():
+  print(stack[-1], end="")
+
+print()
